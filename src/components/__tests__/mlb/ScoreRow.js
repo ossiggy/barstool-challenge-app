49445c11f043491f1f52015260bab41ce666245c
Ossiggy <mrossig201@gmail.com>
2019-11-05 12:58:21 -0500

    finished last test suite in mlb

--- src/components/__tests__/mlb/ScoreRow.js
+++ src/components/__tests__/mlb/ScoreRow.js
@@ -1,4 +1,62 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Score from '../../scoreboard/Score';
+import ScoreRow from "../../mlb/ScoreRow";
 
-import ScoreRow from '../../components/mlb/ScoreRow';
+const data = require("../structures/mlbData.json");
+
+const {
+  stats,
+  totals,
+  home_team,
+  home_period_scores
+} = data;
+
+const headers = new Array(9);
+
+describe("ScoreRow", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <ScoreRow
+        team={home_team}
+        scores={home_period_scores}
+        type="home"
+        hits={totals.home_batter_totals.hits}
+        errors={stats.home._errors}
+        headers={headers}
+      />
+    );
+  });
+
+  it("renders the correct amount of scores", () => {
+    const wrapper = shallow(
+      <ScoreRow
+        team={home_team}
+        scores={home_period_scores}
+        type="home"
+        hits={totals.home_batter_totals.hits}
+        errors={stats.home._errors}
+        headers={headers}
+      />
+    );
+    expect(wrapper.find(Score).length).toEqual(9);
+  });
+
+  it('renders the correct totals', () => {
+    const wrapper = shallow(
+      <ScoreRow
+        team={home_team}
+        scores={home_period_scores}
+        type="home"
+        hits={totals.home_batter_totals.hits}
+        errors={stats.home._errors}
+        headers={headers}
+      />
+    );
+
+    expect(wrapper.find('.totals').length).toEqual(3);
+    expect(wrapper.find('.totals').at(0).text()).toEqual("4");
+    expect(wrapper.find('.totals').at(1).text()).toEqual("10");
+    expect(wrapper.find('.totals').at(2).text()).toEqual("0");
+  })
+});
